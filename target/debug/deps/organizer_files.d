4f7881fe847044c789307d10b2cf8ebfd9106d44
VitalikMk <malyukovvitaliy4@gmail.com>
2023-10-07 11:07:07 +0300

    last

---
+++ target/debug/deps/organizer_files.d
@@ -0,0 +1,5 @@
+D:\proga\Rust_project\organizer_file\organizer_files\target\debug\deps\organizer_files.exe: src\main.rs
+
+D:\proga\Rust_project\organizer_file\organizer_files\target\debug\deps\organizer_files.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/organizer_files.d
@@ -0,0 +1,1 @@
+D:\proga\Rust_project\organizer_file\organizer_files\target\debug\organizer_files.exe: D:\proga\Rust_project\organizer_file\organizer_files\src\main.rs
